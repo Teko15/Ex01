054634a984fe495f5f0b33e03dde1c298ba4d3b8
Eryk <s23741@pjwstk.edu.pl>
2022-03-17 09:17:46 +0100

    init

---
+++ DataExporter/DataExporter/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DataExporter
+{
+    class DataExporter
+    {
+        public void Main(string[] args)
+        {
+            Console.Write("Hiho");
+        }
+    }
+}   
