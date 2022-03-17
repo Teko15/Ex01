a124bfe388de19f00f246ca0b13b8e3d13e859f3
Eryk <s23741@pjwstk.edu.pl>
2022-03-17 12:58:04 +0100

    Po zajeciach z APBD

--- DataExporter/DataExporter/Program.cs
+++ DataExporter/DataExporter/Program.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using DataExporter.Models;
+
 namespace DataExporter
 {
     class DataExporter
     {
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.Write("Hiho");
+            var inputPath = args[0];
+            //var outputPath = args[1];
+
+            //var fileInfo = new FileInfo(inputPath);
+            FileInfo fileInfo = new(inputPath);
+
+            var fileContent = new List<string>();
+
+            using (var streamReader = new StreamReader(fileInfo.OpenRead()))
+            {
+                string line = string.Empty;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    fileContent.Add(line);
+                }
+            }
+            //streamReader.Dispose();
+            foreach (var item in fileContent)
+            {
+                Console.Write(item);
+
+            }
+
+
+            /*
+             * foreach(var line in File.ReadLines(path))
+             * {
+             *  Console.Writeline(line);
+             * }
+             */
+
+            var hashSet = new HashSet<Student>(new MyComparer());
         }
     }
-}   
+}
