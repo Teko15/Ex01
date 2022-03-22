913edfc0451a384fe7980d69411317138c2829f3
Eryk <s23741@pjwstk.edu.pl>
2022-03-22 20:09:54 +0100

    everything excl. exceptions done

---
+++ DataExporter/DataExporter/MyComparer.cs
@@ -0,0 +1,28 @@
+﻿using DataExporter.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataExporter
+{
+    public class MyComparer : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y)
+        {
+            return StringComparer
+                .InvariantCultureIgnoreCase
+                .Equals($"{x.Name} {x.Surname}", $"{y.Name} {y.Surname}");
+        }
+
+        // DateTime - data z czasem
+        public int GetHashCode([DisallowNull] Student obj)
+        {
+            return StringComparer
+                .InvariantCultureIgnoreCase
+                .GetHashCode($"{obj.Name} {obj.Surname}");
+        }
+    }
+}
--- DataExporter/DataExporter/Program.cs
+++ DataExporter/DataExporter/Program.cs
@@ -1,15 +1,42 @@
-﻿using DataExporter.Models;
-
+﻿
 namespace DataExporter
 {
     class DataExporter
     {
+
+        static List<string> allActiveStudies = new List<string>();
+
         public static void Main(string[] args)
         {
-            var inputPath = args[0];
-            //var outputPath = args[1];
+            string log = string.Empty;
+            if (Directory.Exists("log.txt"))
+                log = File.ReadAllText("log.txt");
+            /*
+            if (args.Length != 3)
+            {
+                log += "Invalid number of arguments. " + DateTime.Now + '\n';
+                SaveLogs(log);
+                throw new ArgumentNullException("Invalid number of arguments.");
+            }
 
-            //var fileInfo = new FileInfo(inputPath);
+            if (!Directory.Exists(args[1]))
+            {
+                log += "Podana sciezka jest niepoprawna. " + DateTime.Now + '\n';
+                SaveLogs(log);
+                throw new ArgumentException("Podana sciezka jest niepoprawna");
+            }
+
+            if (!Directory.Exists(args[0]))
+            {
+                log += "Output file doesn't exist. " + DateTime.Now + '\n';
+                SaveLogs(log);
+                throw new FileNotFoundException("Plik {} nie istnieje", args[0]);
+
+            }*/
+
+            var inputPath = args[1];
+            var outputPath = args[0] + '.' + args[2];
+
             FileInfo fileInfo = new(inputPath);
 
             var fileContent = new List<string>();
@@ -18,26 +45,112 @@ namespace DataExporter
             {
                 string line = string.Empty;
                 while ((line = streamReader.ReadLine()) != null)
-                {
-                    fileContent.Add(line);
-                }
+                    fileContent.Add(line + '\n');
             }
-            //streamReader.Dispose();
+
+            var correctData = new List<string>();
+
             foreach (var item in fileContent)
             {
-                Console.Write(item);
+                if (item.Count(e => e == ',') != 8 || item.IndexOf(",,") != -1)
+                    log += item + '\n';
+                else
+                    correctData.Add(item);
+            }
+            string tab = (char) 9 + string.Empty;
+            string tabs = (char)9 + string.Empty + (char)9;
+            string output = "{\n" + 
+                tab + "uczelnia: {\n" + 
+                tab + tab + "createdAt: \"" + DateTime.UtcNow.ToString("dd.MM.yyyy") + "\",\n" +
+                tab + tab + "author: \"Eryk Aftowicz\",\n" +
+                tab + tab + "studenci: [\n";
+            StudentData(correctData[0]);
 
+            for (int i = 0; i < correctData.Count; i++)
+            {
+                output += StudentData(correctData[i]);
+                if (i < correctData.Count - 1)
+                    output += ',';
+                output += '\n';
             }
 
+            output += (tabs + "],\n" +
+                tabs + "activeStudies: [\n");
 
-            /*
-             * foreach(var line in File.ReadLines(path))
-             * {
-             *  Console.Writeline(line);
-             * }
-             */
+            List<String> activeStudies = allActiveStudies.Distinct().ToList();
+            for (int i = 0; i < activeStudies.Count; i++)
+            {
+                output += (tabs + tab + "{\n" +
+                    tabs + tabs + "name: \"" + activeStudies[i] + "\",\n" +
+                    tabs + tabs + "numberOfStudents: " + allActiveStudies.Count(e => e == activeStudies[i]) + "\"\n" +
+                    tabs + tab + '}');
+                if (i < activeStudies.Count - 1)
+                    output += ',';
+                output += '\n';
+            }
+            output += (tabs + "]\n" + tab + "}\n}");
+            Console.WriteLine("Pomyslnie zapisano plik!");
+            File.WriteAllText(outputPath, output);
+        }
 
-            var hashSet = new HashSet<Student>(new MyComparer());
+        static void SaveLogs(string log)
+        {
+            File.WriteAllText("log.txt", log);
+        }
+        static string StudentData(string line)
+        {
+            string tab = (char)9 + string.Empty;
+            string tabs = (char)9 + string.Empty + (char)9 + string.Empty + (char)9;
+            string name = DataFromLine(line);
+            line = CutLine(line);
+            string surname = DataFromLine(line);
+            line = CutLine(line);
+            string studiesName = DataFromLine(line);
+            line = CutLine(line);
+            string studiesMode = DataFromLine(line);
+            line = CutLine(line);
+            string indexNumber = 's' + DataFromLine(line);
+            line = CutLine(line);
+            string birthdate = DataFromLine(line);
+            birthdate = GetGoodDate(birthdate);
+            line = CutLine(line);
+            string email = DataFromLine(line);
+            line = CutLine(line);
+            string mothersName = DataFromLine(line);
+            line = CutLine(line);
+            string fathersName = line[0..^1];
+            allActiveStudies.Add(studiesName);
+            string output = tabs + "{\n" +
+                tabs + tab + "indexNumber: \"" + indexNumber + "\",\n" +
+                tabs + tab + "fname: \"" + name + "\",\n" +
+                tabs + tab + "lname: \"" + surname + "\",\n" +
+                tabs + tab + "birthdate: \"" + birthdate + "\",\n" +
+                tabs + tab + "email: \"" + email + "\",\n" +
+                tabs + tab + "mothersName: \"" + mothersName + "\",\n" +
+                tabs + tab + "fathersName: \"" + fathersName + "\",\n" +
+                tabs + tab + "studies: {\n" +
+                tabs + tab + tab + "name: \"" + studiesName + "\",\n" +
+                tabs + tab + tab + "mode: \"" + studiesMode + "\"\n" +
+                tabs + tab + "}\n" +
+                tabs + '}';
+            return output;
+        }
+        static string DataFromLine(string line)
+        {
+            return line.Substring(0, line.IndexOf(','));
+        }
+        static string CutLine(string line)
+        {
+            return line.Substring(line.IndexOf(',') + 1);
+        }
+
+        static string GetGoodDate(string wrongDate)
+        {
+            string day = wrongDate.Substring(8);
+            string month = wrongDate.Substring(5, 2);
+            string year = wrongDate.Substring(0, 4);
+            string goodDate = day + '.' + month + '.' + year;
+            return goodDate;
         }
     }
 }
